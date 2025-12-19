c639fea09f96eb14a7a1bfeae1b3a91f97d38be8
Khaled <117534908+djay04@users.noreply.github.com>
2025-12-19 10:18:02 -0500

    Fixed POST request for sending email functionality

---
+++ src/app/api/send-email/route.js
@@ -0,0 +1,24 @@
+'use server'
+
+import { Resend } from 'resend'
+
+export default function POST(request){
+
+    const { name, email, message} = request.json()
+
+    const resend = new Resend(process.env.RESEND_API_KEY)
+
+    resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'ksaleh2004@gmail.com',
+        subject: 'Greetings from Coffee Shop!',
+        html: '<p><Thanks for stopping by! Lets continue the chat, book a time here: https://calendly.com/ksaleh2004/30min Have a <strong>Great</strong> day!</p>'
+    })
+
+    return Response.json({
+        name: name,
+        email: email,
+        message: message
+    }, {status: 201})
+
+}
